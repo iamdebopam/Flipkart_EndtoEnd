87f4e54a3696e8825ec22aee8315592b643fedd4
iamdebopam <iamdebopam8@gmail.com>
2025-04-09 20:28:49 +0530

    Day 2 (14:22)

--- src/main/java/com/Baseclass/Library.java
+++ src/main/java/com/Baseclass/Library.java
@@ -1,4 +1,42 @@
 package com.Baseclass;
 
+import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class Library {
+
+    public static WebDriver driver;
+    public static Properties prop;
+    public void launchApplication() throws IOException {
+
+        FileInputStream input=new FileInputStream("src/test/resources/config.Properties");
+        prop = new Properties();
+        prop.load(input);
+
+        try{
+            if(prop.getProperty("browser").equalsIgnoreCase("chrome")){
+                WebDriverManager.chromedriver().setup();
+                driver=new ChromeDriver();
+            } else if (prop.getProperty("browser").equalsIgnoreCase("firefox")) {
+                WebDriverManager.firefoxdriver().setup();
+                driver=new FirefoxDriver();
+            }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            driver.get(prop.getProperty("url"));
+        }
+
+        catch (Exception e){
+            System.out.println("Browser didn't luanch");
+        }
+
+    }
+
 }
